a451a7d1c25c706ef84ca263c371b9a4d6e6a454
Patricio <patriciogatti@gmail.com>
2021-07-15 14:08:23 -0300

    advanced routes examples

--- first-project/src/Controller/DefaultController.php
+++ first-project/src/Controller/DefaultController.php
@@ -22,9 +22,9 @@ class DefaultController extends AbstractController
      * @Route("/", name="default")
      */
     public function index(GiftsService $gifts): Response
-    {                   
+    {
         // $users = ['Carlos','Fede','Pia','Iván','Nacho'];
-                
+
         // $entityManager = $this->getDoctrine()->getManager();
 
         // $user = new User;
@@ -37,13 +37,13 @@ class DefaultController extends AbstractController
         // $user4->setName('Iván');
         // $user5 = new User;
         // $user5->setName('Nacho');
-        
+
         // $entityManager->persist($user);
         // $entityManager->persist($user2);
         // $entityManager->persist($user3);
         // $entityManager->persist($user4);
         // $entityManager->persist($user5);
-        
+
         // $entityManager->flush();
 
         // bin/console make:migration
@@ -58,7 +58,46 @@ class DefaultController extends AbstractController
         ]);
     }
 
-    
+    /** 
+     * @Route ("/blog/{page?}", name="blog_list", requirements={"page"="\d+"})
+     * 
+     */
+    public function index2(): Response
+    {
+        // needs a number as parameter, with question mark it's optional
+        return new Response('Optional parameters in url and requirements for this');
+    }
+
+    /**
+     * @Route(
+     *      "/articles/{_locale}/{year}/{slug}/{category}",
+     *      defaults={"category": "computers"},
+     *      requirements={
+     *         "_locale": "en|fr",
+     *         "category": "computers|rtv",
+     *         "year": "\d+"
+     *      }
+     * )
+     */
+    public function index3(): Response
+    {
+        return new Response('An advanced route example');
+    }
+
+    /**
+     * @Route({
+     *      "nl": "/over-ons", 
+     *      "en": "/about-us"
+     *      },
+     *      name="about_us"
+     * )
+     */
+    public function index4(): Response
+    {
+        return new Response('Translated routes');
+    }
+
+
     // /**
     //  * @Route("/default/{name}", name="default")
     //  */
@@ -69,7 +108,7 @@ class DefaultController extends AbstractController
     //     ]);
 
     //     return $this->json(['username'=>'john.doe']);
-    
+
     //     return new Response("Hello! $name");
 
     //     return $this->redirect('http://synfony.com');
