5abca95b66694d6d87645f709149124f0f3020d1
Patricio <patriciogatti@gmail.com>
2021-07-15 13:46:38 -0300

    Logger and favicon

--- first-project/src/Services/GiftsService.php
+++ first-project/src/Services/GiftsService.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Services;
 
+use Psr\Log\LoggerInterface;
+
 class GiftsService
 {
   public $gifts = ['flowers','car','piano','money','bricks'];
     
-  public function __construct() {
+  public function __construct(LoggerInterface $logger)
+  {
+    $logger->info('Gifts were randomized'); // var/cache/log/dev.log
+
     shuffle($this->gifts);
   }
 }
